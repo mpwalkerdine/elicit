41bc07532384684fabf5b9b6dcd969a147439792
Matt Walkerdine <mpwalkerdine@googlemail.com>
2017-02-01 19:51:42 +0000

    Separate Spec and Scenario Contexts
    In preparation for holding data at the two levels
    (e.g. tables), two new structures have been created
    which are children of the overall context.

--- elicit.go
+++ elicit.go
@@ -38,43 +38,53 @@ const (
 	Failed
 )
 
+type specContext struct {
+	name string
+}
+
+type scenarioContext struct {
+	name        string
+	currentStep string
+	skipped     bool
+	failed      bool
+	logbuf      bytes.Buffer
+}
+
 // Context stores test machinery and maintains state between specs/scenarios/steps
 type Context struct {
-	steps        map[*regexp.Regexp]interface{}
-	transforms   map[*regexp.Regexp]StepArgumentTransform
-	specName     string
-	scenarioName string
-	current      string
-	skipped      bool
-	failed       bool
-	logbuf       bytes.Buffer
+	steps      map[*regexp.Regexp]interface{}
+	transforms map[*regexp.Regexp]StepArgumentTransform
+	spec       specContext
+	scenario   scenarioContext
 }
 
 // BeginSpecTest registers the start of Spec
 func (e *Context) BeginSpecTest(name string) {
-	e.specName = name
-	e.logSpecStart()
+	e.spec = specContext{
+		name: name,
+	}
 }
 
 // BeginScenarioTest registers the start of a Scenario
 func (e *Context) BeginScenarioTest(name string) {
-	e.scenarioName = name
-	e.skipped = false
-	e.failed = false
-	e.logbuf = bytes.Buffer{}
+	e.scenario = scenarioContext{
+		name:    name,
+		skipped: false,
+		failed:  false,
+		logbuf:  bytes.Buffer{},
+	}
 	e.logScenarioStart()
 }
 
 // EndScenarioTest marks the end of a scenario and signals the outcome
 func (e *Context) EndScenarioTest() (Result, string) {
+	log := string(e.scenario.logbuf.Bytes())
 
-	log := string(e.logbuf.Bytes())
-
-	if e.failed {
+	if e.scenario.failed {
 		return Failed, log
 	}
 
-	if e.skipped {
+	if e.scenario.skipped {
 		return Skipped, log
 	}
 
@@ -129,7 +139,7 @@ func ensureCompleteMatch(pattern string) string {
 
 // RunStep matches stepText to a registered step implementation and invokes it
 func (e *Context) RunStep(stepText string) {
-	e.current = stepText
+	e.scenario.currentStep = stepText
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -143,13 +153,13 @@ func (e *Context) RunStep(stepText string) {
 
 		if in, ok := convertParams(f, params); ok {
 
-			if !e.skipped && !e.failed {
+			if !e.scenario.skipped && !e.scenario.failed {
 				f.Call(in)
 			} else {
 				e.Skip("")
 			}
 
-			if !e.skipped && !e.failed {
+			if !e.scenario.skipped && !e.scenario.failed {
 				e.stepPassed()
 			}
 
@@ -213,12 +223,12 @@ func convertParam(s string, target reflect.Type) (reflect.Value, bool) {
 // Skip skips the current step execution and all subsequent steps
 func (e *Context) Skip(format string, args ...interface{}) {
 	e.logStepResult("⤹", format, args...)
-	e.skipped = true
+	e.scenario.skipped = true
 }
 
 func (e *Context) stepNotFound() {
 	e.logStepResult("?", "")
-	e.skipped = true
+	e.scenario.skipped = true
 }
 
 func (e *Context) stepPassed() {
@@ -233,22 +243,18 @@ func (e *Context) Fail() {
 // Failf logs the supplied message and records test step failure
 func (e *Context) Failf(format string, args ...interface{}) {
 	e.logStepResult("✘", format, args...)
-	e.failed = true
-}
-
-func (e *Context) logSpecStart() {
-	fmt.Fprintf(&e.logbuf, "%s\n", e.specName)
+	e.scenario.failed = true
 }
 
 func (e *Context) logScenarioStart() {
-	fmt.Fprintf(&e.logbuf, "\n\t%s\n", e.scenarioName)
+	fmt.Fprintf(&e.scenario.logbuf, "\n\t%s\n", e.scenario.name)
 }
 
 func (e *Context) logStepResult(prefix, format string, args ...interface{}) {
 	if len(format) > 0 {
-		format = fmt.Sprintf("\t\t%s %s\t(%s)\n", prefix, e.current, format)
-		fmt.Fprintf(&e.logbuf, format, args...)
+		format = fmt.Sprintf("\t\t%s %s\t(%s)\n", prefix, e.scenario.currentStep, format)
+		fmt.Fprintf(&e.scenario.logbuf, format, args...)
 	} else {
-		fmt.Fprintf(&e.logbuf, "\t\t%s %s\n", prefix, e.current)
+		fmt.Fprintf(&e.scenario.logbuf, "\t\t%s %s\n", prefix, e.scenario.currentStep)
 	}
 }
